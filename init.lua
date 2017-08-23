4cb79fdacd99af970b4b9fbe02f439a1e6dfae51
crisnnik <31289241+crisnnik@users.noreply.github.com>
2017-08-23 14:30:04 -0400

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,25 @@
+function startup()
+    if file.open("init.lua") == nil then
+        print("init.lua deleted or renamed")
+    else
+        print("Running")
+        file.close("init.lua")
+        dofile("application.lua")
+    end
+end
+
+print("Connecting to WiFi access point...")
+wifi.setmode(wifi.STATION)
+wifi.sta.config('CASTLEWIFI', 'B0st0nR3dS0x')
+wifi.sta.connect()
+tmr.alarm(1, 1000, 1, function()
+    if wifi.sta.getip() == nil then
+        print("Waiting for IP address...")
+    else
+        tmr.stop(1)
+        print("WiFi connection established, IP address: " .. wifi.sta.getip())
+        print("You have 3 seconds to abort")
+        print("Waiting...")
+        tmr.alarm(0, 3000, 0, startup)
+    end
+end)
